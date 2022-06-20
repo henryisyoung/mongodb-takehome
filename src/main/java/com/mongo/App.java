1f644fd0924a6e5ff08306ef07016003d1886395
yunz <zyun3939@gmail.com>
2022-06-20 10:39:27 -0700

    finsih tests

--- src/main/java/com/mongo/App.java
+++ src/main/java/com/mongo/App.java
@@ -1,45 +1,80 @@
 package com.mongo;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import org.json.simple.*;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, Charset.defaultCharset()))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println("Read: " + line);
                 sb.append(line);
             }
-        }
-        String json = sb.toString();
-        System.out.println(json);
-        ObjectMapper mapper = new ObjectMapper();
+            String json = sb.toString().replaceAll("\\s", "");
+            System.out.println("Before Formatting: " + json);
 
-        try {
-
-            // convert JSON string to Map
-            Map<String, String> map = mapper.readValue(json, Map.class);
-
-            // it works
-            //Map<String, String> map = mapper.readValue(json, new TypeReference<Map<String, String>>() {});
-
-            System.out.println(map);
-
-            json = mapper.writeValueAsString(map);
-
-            System.out.println(json);   // compact-print
-
-            json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
-
-            System.out.println(json);
-
+            System.out.println("After Formatting:" + reformatJson(json));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("System IO Exception: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+
+    public static String reformatJson(String json) {
+        if (json == null || json.length() == 0) return "";
+        Object obj= JSONValue.parse(json);
+        JSONObject jsonObject = (JSONObject) obj;
+        Iterator<String> keys = jsonObject.keySet().iterator();
+
+        Map<String, Object> map = new HashMap();
+        while(keys.hasNext()) {
+            String key = keys.next();
+            Map<String, Object> segmentMap = compressJson(key, jsonObject.get(key));
+            for (Map.Entry<String, Object> entry : segmentMap.entrySet()) {
+                map.put(entry.getKey(), entry.getValue());
+            }
+        }
+        // return compressed json string
+        String jsonText = JSONValue.toJSONString(map);
+
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return jsonText;
+    }
+
+    private static Map<String, Object> compressJson(String key, Object obj) {
+        if (obj instanceof JSONObject) {
+            // recursively DFS the end value, if obj is a json object
+            JSONObject jsonObject = (JSONObject) obj;
+            Iterator<String> childrenKeys = jsonObject.keySet().iterator();
+            Map<String, Object> result = new HashMap();
+
+            // iterate all next children json object
+            while(childrenKeys.hasNext()) {
+                String childKey = childrenKeys.next();
+                Map<String, Object> map = compressJson(childKey, jsonObject.get(childKey));
+                for (String mapKey : map.keySet()) {
+                    // concat new compressed key
+                    String newKey = key + "." + mapKey;
+                    result.put(newKey, map.get(mapKey));
+                }
+            }
+            return result;
+        } else {
+            // this is the end of search, bubble up the end value
+            Map<String, Object> map = new HashMap();
+            map.put(key, obj);
+            return map;
         }
     }
 }
--- src/test/java/com/mongo/AppTest.java
+++ src/test/java/com/mongo/AppTest.java
@@ -5,16 +5,34 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for Json compressor App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void reformatJsonCompressValidJson() {
+        String json = "{\"a\":1,\"b\":true,\"c\":{\"d\":3,\"e\":\"test\"}}";
+        String formattedJson = App.reformatJson(json);
+        String compressedJson = "{\"a\":1,\"b\":true,\"c.d\":3,\"c.e\":\"test\"}";
+        assertTrue( formattedJson.equals(compressedJson) );
+
+        String json2 = "{\"a\":1,\"b\":true,\"c\":{\"d\":3,\"e\":\"test\",\"f\":{\"g\":3,\"h\":{\"asd\":123,\"fggdfs\":true,\"asdsdfsdf\":123.1}}}}";
+        String formattedJson2 = App.reformatJson(json2);
+        String compressedJson2 = "{\"a\":1,\"b\":true,\"c.d\":3,\"c.f.h.fggdfs\":true,\"c.e\":\"test\",\"c.f.h.asdsdfsdf\":123.1,\"c.f.g\":3,\"c.f.h.asd\":123}";
+        assertTrue( formattedJson2.equals(compressedJson2) );
+    }
+
+    @Test
+    public void reformatJsonCompressEmptyJson() {
+        String json = "{}";
+        String formattedJson = App.reformatJson(json);
+        String compressedJson = "{}";
+        assertTrue( formattedJson.equals(compressedJson) );
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void reformatJsonCompressInvalidJson() {
+        String json = "{this is not a valid json}";
+        App.reformatJson(json);
     }
 }
